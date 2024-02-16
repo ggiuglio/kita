273c823eeb1735aa1672fb8b37ed39a4c32d47f5
Giulio Gherardi <giulio.gherardi@mercanis.com>
2024-02-16 15:57:19 +0100

    history page translations

--- src/routes/(app)/geschichte/+page.svelte
+++ src/routes/(app)/geschichte/+page.svelte
@@ -47,53 +47,67 @@
     <section id="geschichte" class="pt-5">
       <div class="container padding-medium pt-0">
   
-      <div class="banner-content text-center m-auto" data-aos="zoom-out">
-        <h2 class="display-5 mb-4">
-            Wir wollten doch bloß spielen
-        </h2>
-      </div>
-      <p>
-        Zunächst ahnten wir nicht, wie viele italienische Mamas oder Papas mit ihren Kindern im Prenzlauer Berg leben.
-         Doch bald fanden immer mehr Eltern zueinander und trafen sich in der deutsch-italienischen Spielgruppe  „La Nave Gelsomina“ .
-      </p>
+        <div class="banner-content text-center m-auto" data-aos="zoom-out">
+          <h2 class="display-6 mb-2">
+            {$t("history.title-1")}
+          </h2>
+        </div>
 
-      <div class="banner-content text-center m-auto mt-8" data-aos="zoom-out">
-        <h2 class="display-5 mb-4 pt-5">
-            Gemeinsam lösen wir die K-Frage
-        </h2>
-      </div>
-      <p>
-        <b>"Habt ihr auch noch keinen Kita-Platz? Und wäre es nicht schön, wenn unsere Kinder auch mit Papas oder Mamas ,Muttersprache' aufwachsen würden?“</b>  
-        Die Idee war geboren: Wir eröffnen eine eigene Kita! Dort kann jeder italienisch UND deutsch sprechen. Ist das nicht verrückt? Ganz und gar nicht - und doch ein bisschen.
+        <p>
+          {$t("history.text-1")}
+          <a href="http://it-it.facebook.com/lanavegelsomina"> {$t("history.link-1")}</a>
         </p>
-      <p class="mb-4">
-        Gesagt getan - im Sommer 2010 haben sich einige Familien zum Verein „Folli Folletti e.V.“ 
-        zusammengetan und nach arbeitsreichen Monaten viel erreicht: Mit Freude und Stolz blicken wir auf die gelungene Eröffnung der Kita 
-        in der Cantianstraße 15 am 8. September 2011 zurück. In unserer Kita betreuen vier qualifizierte Erzieherinnen und Erzieher italienischer 
-        und deutscher Herkunft zwanzig „verrückte Wichtel“ zwischen ein und sechs Jahren, dazu kommen Zivildienstleistende, Praktikanten und Auszubildende, 
-        so dass wir oft einen Betreuungsschlüssel von 1:3 haben.
-      </p>
-     
-      <div class="banner-content text-center m-auto" data-aos="zoom-out">
-        <h2 class="display-5 mb-4 pt-5">
-            In Kürze: Das ist uns wichtig
-        </h2>
-      </div>
-      <p>
-        Wir haben einen kreativen, bilingualen, bewegungsorientierten, umweltbewussten und familiären Kindergarten gebaut. Als Elterninitiativkita (EKT) setzen wir auf das 
-        Engagement der Eltern sowie auf Transparenz und einen guten Dialog zwischen Erziehern und Eltern. Zusätzlich verstehen wir den Verein und die Kita als Treffpunkt für alle, 
-        die an einem Austausch zwischen deutscher und italienischer Kultur interessiert sind
-        <a href="/konzeption">Die Konzeption finden Sie hier (Stand Oktober 2013????)</a>.
-      </p>
-      <p>
-        Kleine Beiträge - große Wirkung Ohne den Einsatz von vielen Menschen wäre unsere Kita undenkbar. Vielen Dank an alle kleinen und großen Hände, 
-        die kraftvoll angepackt haben und zum Erfolg unseres Projekts beigetragen.
-      </p>
-      <p>
-        Sie können Fördermitglied werden, indem Sie diesen <a>Aufnahmeantrag</a> ausdrucken und ihn per Email oder per Post an uns schicken.
-        Gerne können Sie den Vorstand des Vereins kontaktieren (info[at]folli-folletti.de), der Ihnen für weitere Fragen zur Verfügung steht. 
-        <a>Die Satzung unseres Vereins können Sie hier</a> nachlesen.
-      </p>
+
+        <div class="banner-content text-center m-auto mt-8" data-aos="zoom-out">
+          <h2 class="display-6 mb-2 pt-3">
+            {$t("history.title-2")}
+          </h2>
+        </div>
+        
+        <p>
+          <b>
+            " {$t("history.text-2")} “
+          </b>  
+          <br />
+          {$t("history.text-3")}         
+        </p>
+
+        <p>
+          {$t("history.text-4")} 
+        </p>
+      
+        <div class="banner-content text-center m-auto" data-aos="zoom-out">
+          <h2 class="display-6 pt-3">
+            {$t("history.title-3")} 
+          </h2>
+        </div>
+
+        <p>
+          {$t("history.text-5")} 
+          <a href="/http://www.tobikun.com/folletti/FolliFolletti_files/Konzeption_StandOktober2013.pdf">          
+            {$t("history.link-2")} 
+          </a>
+        </p>
+
+        <div class="banner-content text-center m-auto" data-aos="zoom-out">
+          <h2 class="display-6 pt-3">
+            {$t("history.title-4")}
+          </h2>
+        </div>
+        <p>
+          {$t("history.text-7")}
+        </p>
+        <p>
+          {$t("history.text-8")}
+          <a href="http://www.tobikun.com/folletti/FolliFolletti_files/Antrag_auf_Vereinsmitgliedschaft.doc">
+            {$t("history.link-3")}
+          </a>
+          {$t("history.text-9")}
+          <a href="http://www.tobikun.com/folletti/FolliFolletti_files/KiTa_Satzung.doc">
+            {$t("history.link-4")}
+          </a>
+          {$t("history.text-10")}
+        </p>
       </div>
     </section>
   </div>
